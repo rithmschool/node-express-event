c30521ec94d6a17f116910ddd87b305ae67e5c5c
Michael Hueter <mthueter@gmail.com>
2018-11-13 19:44:15 -0800

    error handling

--- index.js
+++ index.js
@@ -23,12 +23,53 @@ app.get('/greet/:name', function(req, res, next) {
   res.send('This is a greeting. Hi ' + name);
 });
 
-app.get('/add/:num1/:num2', function(req, res, next) {
-  var firstNum = Number(req.params.num1);
-  var secondNum = Number(req.params.num2);
-  var sum = firstNum + secondNum;
-  res.send('The sum is ' + sum);
+app.get('/:operation/:num1/:num2', function(req, res, next) {
+  var op = req.params.operation;
+  var num1 = Number(req.params.num1);
+
+  if (isNaN(num1)) {
+    var error = new Error('num1 has to be a proper number.');
+    return next(error);
+  }
+  var num2 = Number(req.params.num2);
+  if (isNaN(num2)) {
+    var error = new Error('num2 has to be a proper number.');
+    return next(error);
+  }
+
+  var result;
+
+  if (op === 'add') {
+    result = num1 + num2;
+  } else if (op === 'subtract') {
+    result = num1 - num2;
+  } else if (op === 'divide') {
+    result = num1 / num2;
+  } else if (op === 'multiply') {
+    result = num1 * num2;
+  }
+
+  res.send('The result is ' + result);
 });
+
+// ERROR HANDLER
+app.use(function(error, req, res, next) {
+  res.send('SOMETHING BAD HAPPENED: ' + error.message);
+});
+
+// app.get('/add/:num1/:num2', function(req, res, next) {
+//   var firstNum = Number(req.params.num1);
+//   var secondNum = Number(req.params.num2);
+//   var sum = firstNum + secondNum;
+//   res.send('The sum is ' + sum);
+// });
+
+// app.get('/subtract/:num1/:num2', function(req, res, next) {
+//   var firstNum = Number(req.params.num1);
+//   var secondNum = Number(req.params.num2);
+//   var difference = firstNum - secondNum;
+//   res.send('The difference is ' + difference);
+// });
 
 app.listen(3000, function() {
   console.log('Express server listening on Port 3000');
